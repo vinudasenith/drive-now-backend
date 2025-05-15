a71f842965dbb09bb34dbeb2908300de3223c9b1
vinudasenith <ha.vinudas@gmail.com>
2025-05-15 17:43:03 +0530

    implement order modle✅

---
+++ models/order.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose"
+
+const orderSchema = new mongoose.Schema({
+    orderId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    orderDate: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    orderedItems: [
+        {
+            product: {
+                key: { type: String, required: true },
+                name: { type: String, required: true },
+                image: { type: String, required: true },
+                price: { type: Number, required: true }
+            },
+            quantity: { type: Number, required: true }
+        }
+    ],
+    days: {
+        type: Number,
+        required: true
+    },
+    startingDate: {
+        type: Date,
+        required: true
+    },
+    endingDate: {
+        type: Date,
+        required: true
+    },
+    isApproved: {
+        type: Boolean,
+        default: false,
+        required: true
+    },
+    status: {
+        type: String,
+        default: "Pending",
+        required: true
+    },
+    totalAmount: {
+        type: Number,
+        required: true
+    }
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order
