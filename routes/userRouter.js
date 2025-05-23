95c647881fa18f78f6887a28889bd383d70646b0
vinudasenith <ha.vinudas@gmail.com>
2025-05-23 08:21:17 +0530

    done

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/userController.js";
 
+
 const userRouter = express.Router()
 
 userRouter.post("/", registerUser);
