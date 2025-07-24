cf684bbd5d799ba428896460aa6614fa84f696c3
vinudasenith <ha.vinudas@gmail.com>
2025-07-24 07:57:36 +0530

    restrcture👍

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser, loginUser, getAllUsers, blockOrUnblockUser } from "../controllers/UserController";
+import { registerUser, loginUser, getAllUsers, blockOrUnblockUser } from "../controllers/userController.js";
 
 
 const userRouter = express.Router()
